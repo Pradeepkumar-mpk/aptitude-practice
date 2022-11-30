1cf0e059e487d4c55e5a572c049250a7caf2dffb
Admin <admin@Tanveers-MacBook-Air.local>
2022-11-30 06:16:08 +0530

    add practice programs.

---
+++ 45/js/45.js
@@ -0,0 +1,6 @@
+
+var dis=parseInt(prompt("enter dis"));
+var time=parseInt(prompt("enter time"));
+var speed;
+var speed= dis/time;
+alert (speed);
---
+++ 47/js/47.js
@@ -0,0 +1,5 @@
+var x = [468,52,6,76,102];
+var y = x[0]+x[1]+x[2]+x[3]+x[4];
+var z = x.length;
+var avg = y/z;
+ alert(avg);
